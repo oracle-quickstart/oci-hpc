16545ee55a3359c124d2f7d44da18321e1413702
Marcin Zablocki <marcin.zablocki@oracle.com>
2020-03-02 11:38:17 -0800

    Updated site.yml Update configure.sh for new inventory file.

--- configure.sh
+++ configure.sh
@@ -47,20 +47,20 @@ forks=$(($threads * 8))
 sudo sed -i "s/^#forks.*/forks = ${forks}/" /etc/ansible/ansible.cfg
 sudo sed -i "s/^#fact_caching=.*/fact_caching=jsonfile/" /etc/ansible/ansible.cfg
 sudo sed -i "s/^#fact_caching_connection.*/fact_caching_connection=\/tmp\/ansible/" /etc/ansible/ansible.cfg
-
+sudo mv /home/opc/playbooks/inventory /etc/ansible/hosts
 #
 # Ansible will take care of key exchange and learning the host fingerprints, but for the first time we need
 # to disable host key checking. 
 #
 
 if [[ $execution -eq 1 ]] ; then
-  ANSIBLE_HOST_KEY_CHECKING=False ansible all -m setup --tree /tmp/ansible -i /home/opc/playbooks/inventory
-  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml -i /home/opc/playbooks/inventory
+  ANSIBLE_HOST_KEY_CHECKING=False ansible all -m setup --tree /tmp/ansible 
+  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml 
 else
 
 	cat <<- EOF > /tmp/motd
 	At least one of the cluster nodes has been innacessible during installation. Please validate the hosts and re-run: 
-	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml -i /home/opc/playbooks/inventory
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml 
 EOF
 
 sudo mv /tmp/motd /etc/motd
