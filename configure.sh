1ac302649f28f2e97c12b652fa06e091b7ad6a36
Marcin Zablocki <32143539+MarcinZablocki@users.noreply.github.com>
2020-12-18 15:11:28 -0800

    Updated configuration logic

--- configure.sh
+++ configure.sh
@@ -29,36 +29,36 @@ sudo sed -i "s/^#stdout_callback.*/stdout_callback=yaml/" /etc/ansible/ansible.c
 
 sudo mv /home/opc/playbooks/inventory /etc/ansible/hosts
 
-if [ -f /tmp/configure ] ; then 
-	configure = $(cat /tmp/configure)
-else 
-	configure = True
-fi 
+if [ -f /tmp/configure.conf ] ; then
+        configure=$(cat /tmp/configure.conf)
+else
+        configure=true
+fi
 
-if [[ $configure != True ]] ; then
-	echo "Do not configure is set. Exiting"
-	exit
-fi 
+if [[ $configure != true ]] ; then
+        echo "Do not configure is set. Exiting"
+        exit
+fi
 
 #
-# A little waiter function to make sure all the nodes are up before we start configure 
+# A little waiter function to make sure all the nodes are up before we start configure
 #
 
-echo "Waiting for SSH to come up" 
+echo "Waiting for SSH to come up"
 
 for host in $(cat /tmp/hosts) ; do
-  r=0 
+  r=0
   echo "validating connection to: ${host}"
   while ! ssh ${ssh_options} opc@${host} uptime ; do
 
-	if [[ $r -eq 10 ]] ; then 
-		  execution=0
-		  break
-	fi 
-    	  
-	  echo "Still waiting for ${host}"
-          sleep 60 
-	  r=$(($r + 1))
+        if [[ $r -eq 10 ]] ; then
+                  execution=0
+                  break
+        fi
+
+          echo "Still waiting for ${host}"
+          sleep 60
+          r=$(($r + 1))
 
   done
 done
@@ -68,19 +68,19 @@ done
 
 #
 # Ansible will take care of key exchange and learning the host fingerprints, but for the first time we need
-# to disable host key checking. 
+# to disable host key checking.
 #
 
 if [[ $execution -eq 1 ]] ; then
   ANSIBLE_HOST_KEY_CHECKING=False ansible all -m setup --tree /tmp/ansible > /dev/null 2>&1
-  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml 
+  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml
 else
 
-	cat <<- EOF > /tmp/motd
-	At least one of the cluster nodes has been innacessible during installation. Please validate the hosts and re-run: 
-	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml 11
+        cat <<- EOF > /tmp/motd
+        At least one of the cluster nodes has been innacessible during installation. Please validate the hosts and re-run:
+        ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/opc/playbooks/site.yml 11
 EOF
 
 sudo mv /tmp/motd /etc/motd
 
-fi 
+fi
