d9e05fa25763e91b63e6285c4e34c7421e813d2d
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-01-20 00:30:15 +0100

    Add Maximum number of nodes to spin up

--- autoscaling/crontab/autoscale_slurm.sh
+++ autoscaling/crontab/autoscale_slurm.sh
@@ -6,6 +6,7 @@ import time
 import sys, os
 path = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
 clusters_path = os.path.join(path,'clusters')
+max_number_nodes=20
 cluster_names_number=10
 hpc_cluster_names_number=30
 shapes={'VM.GPU2.1':'gpu21','BM.GPU2.2':'gpu22','VM.GPU3.1':'gpu31','VM.GPU3.2':'gpu32','VM.GPU3.4':'gpu34','BM.GPU3.8':'gpu38','BM.GPU4.8':'gpu48',\
@@ -33,11 +34,13 @@ def getstatus_slurm():
                 cluster_to_build.append([int(line.split()[3]),int(line.split()[1]),shape,CN])
 
     cluster_to_destroy=[]
-    out = subprocess.Popen(['sinfo','-o','\"%T %E %N\"'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    current_nodes=0
+    out = subprocess.Popen(['sinfo','-o','\"%T %E %D %N\"'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout,stderr = out.communicate()
     for line in stdout.split("\n")[1:]:
         if len(line.split()) == 0:
             break
+        current_nodes+=int(line.split()[2])
         if line.split()[0] == '\"idle':
             nodes=line.split()[-1].split(',')
             clusters = []
@@ -69,22 +72,23 @@ def getstatus_slurm():
                 CN = line.split()[2]
             try:
                 cluster_building.append([int(nodes),shape,CN])
+                current_nodes+=int(nodes)
             except ValueError:
                 print 'The cluster '+ clusterName + ' does not have a valid entry for \"currently_building\"'
                 print 'Ignoring'
                 continue
         if os.path.isfile(os.path.join(clusters_path,clusterName,'currently_destroying')):
             cluster_destroying.append(clusterName)
-    return cluster_to_build,cluster_to_destroy,cluster_building,cluster_destroying,available_names
+    return cluster_to_build,cluster_to_destroy,cluster_building,cluster_destroying,available_names,current_nodes
 
-cluster_to_build,cluster_to_destroy,cluster_building,cluster_destroying,available_names=getstatus_slurm()
+cluster_to_build,cluster_to_destroy,cluster_building,cluster_destroying,available_names,current_nodes=getstatus_slurm()
 
 print time.strftime("%Y-%m-%d %H:%M:%S")
 print cluster_to_build,'cluster_to_build'
 print cluster_to_destroy,'cluster_to_destroy'
 print cluster_building,'cluster_building'
 print cluster_destroying,'cluster_destroying'
-
+print current_nodes,'current_nodes'
 
 for i in cluster_building:
     for j in cluster_to_build:
@@ -95,7 +99,11 @@ for index,cluster in enumerate(cluster_to_build):
     shape = cluster[2]
     keyworkShape=shapes[shape]
     clusterName=available_names[keyworkShape][index]+'-'+keyworkShape
-    subprocess.Popen([path+'/create_cluster.sh',str(cluster[0]),clusterName,cluster[2],cluster[3]])
-    time.sleep(10)
+    if current_nodes + cluster[0]> max_number_nodes:
+        print "Cluster "+clusterName+" won't be created, it would go over the total number of nodes limit"
+    else:
+        current_nodes+=cluster[0]
+        subprocess.Popen([path+'/create_cluster.sh',str(cluster[0]),clusterName,cluster[2],cluster[3]])
+        time.sleep(10)
 for cluster in cluster_to_destroy:
     subprocess.Popen([path+'/delete_cluster.sh',str(cluster)])
