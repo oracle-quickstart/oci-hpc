7980c8cadc5c7411a38a002e895a411479545a79
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-01-26 10:48:30 +0100

    Fix Instance pool Autoscaling

--- instance-pool.tf
+++ instance-pool.tf
@@ -19,7 +19,7 @@ resource "oci_core_volume_attachment" "instance_pool_volume_attachment" {
 
 
 resource "oci_core_instance_pool" "instance_pool" {
-  count = var.cluster_network ? 0 : 1
+  count = var.cluster_network || var.node_count == 0 ? 0 : 1
   depends_on     = [oci_core_app_catalog_subscription.mp_image_subscription, oci_core_subnet.private-subnet, oci_core_subnet.public-subnet]
   compartment_id = var.targetCompartment
   instance_configuration_id = oci_core_instance_configuration.instance_pool_configuration[0].id
