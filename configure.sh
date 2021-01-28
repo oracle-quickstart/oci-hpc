7bb08dfd38f163575c4e0ea62008de467c0345e2
Marcin Zablocki <marcin.zablocki@oracle.com>
2021-01-28 14:44:08 -0800

    Split and parallelize provisioning

--- configure.sh
+++ configure.sh
@@ -9,24 +9,6 @@
 execution=1
 
 ssh_options="-i ~/.ssh/cluster.key -o StrictHostKeyChecking=no"
-sudo cloud-init status --wait
-#
-# Install ansible and other required packages
-#
-sudo yum makecache
-sudo yum install -y ansible python-netaddr
-
-ansible-galaxy collection install community.general
-ansible-galaxy collection install ansible.posix
-
-threads=$(nproc)
-forks=$(($threads * 8))
-
-sudo sed -i "s/^#forks.*/forks = ${forks}/" /etc/ansible/ansible.cfg
-sudo sed -i "s/^#fact_caching=.*/fact_caching=jsonfile/" /etc/ansible/ansible.cfg
-sudo sed -i "s/^#fact_caching_connection.*/fact_caching_connection=\/tmp\/ansible/" /etc/ansible/ansible.cfg
-sudo sed -i "s/^#bin_ansible_callbacks.*/bin_ansible_callbacks=True/" /etc/ansible/ansible.cfg
-sudo sed -i "s/^#stdout_callback.*/stdout_callback=yaml/" /etc/ansible/ansible.cfg
 
 sudo mv /home/opc/playbooks/inventory /etc/ansible/hosts
 
