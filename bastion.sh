01b2071470fff7d247f2ba4a11e1961b6fe2215c
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-02-22 23:58:12 +0100

    Revert back to yum makecache

--- bastion.sh
+++ bastion.sh
@@ -10,11 +10,10 @@ execution=1
 
 ssh_options="-i ~/.ssh/cluster.key -o StrictHostKeyChecking=no"
 sudo cloud-init status --wait
-sleep 30
 #
 # Install ansible and other required packages
 #
-# sudo yum makecache
+sudo yum makecache
 sudo yum install -y ansible python-netaddr
 
 ansible-galaxy collection install ansible.netcommon > /dev/null
