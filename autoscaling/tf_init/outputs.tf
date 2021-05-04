06ed6e0f9e3ff3fcb3108ee09136e776ed1a69b9
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-05-04 23:28:50 +0200

    Fix issue with Instance Pool on Autoscaling

--- autoscaling/tf_init/outputs.tf
+++ autoscaling/tf_init/outputs.tf
@@ -8,6 +8,6 @@ output "ocids" {
   value = join(",", local.cluster_instances_ids)
 }
 output "cluster_ocid" {
-  value = oci_core_cluster_network.cluster_network[0].id
+  value = var.cluster_network ? oci_core_cluster_network.cluster_network[0].id : oci_core_instance_pool.instance_pool[0].id
 }
 
