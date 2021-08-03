32d641e17e9d99257174b17988bf5b2916c9da7b
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-08-03 12:53:45 +0200

    check for duplicate instance_keyword line

--- autoscaling/slurm_config.sh
+++ autoscaling/slurm_config.sh
@@ -3,8 +3,14 @@
 # Regenerate Slurm Config
 #
 
+
 scripts=`realpath $0`
 folder=`dirname $scripts`
 playbooks_path=$folder/../playbooks/
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config.yml
+if [[ `cat $folder/queues.conf | grep instance_keyword |  uniq -c -d | wc -l ` == 0 ]];
+then
+   ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config.yml
+else
+   echo "There are some duplicates instance_keyword lines, please make them unique"   
+fi
