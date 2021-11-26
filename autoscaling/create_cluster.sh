0e1bb656e0c7e169e823031eb785f82bf5f75f30
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-11-26 23:54:15 +0100

    Remove node index in monitoring

--- autoscaling/create_cluster.sh
+++ autoscaling/create_cluster.sh
@@ -69,7 +69,7 @@ if [ $status -eq 0 ]
         hostname_array+=( $hostname )
       done
       for index in "${!ip_array[@]}"; do
-          mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; UPDATE cluster_log.nodes SET created='$end_timestamp',state='running',hostname='${hostname_array[$index]}',ip='${ip_array[$index]}',node_OCID='${ocid_array[$index]}' WHERE cluster_id='$2_${date}' AND cluster_index=$(($index+1));" >> $folder/logs/create_$2_${date}.log 2>&1
+          mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; UPDATE cluster_log.nodes SET created='$end_timestamp',state='running',hostname='${hostname_array[$index]}',ip='${ip_array[$index]}',node_OCID='${ocid_array[$index]}' WHERE cluster_id='$2_${date}';" >> $folder/logs/create_$2_${date}.log 2>&1
       done
     fi
   else
@@ -97,7 +97,7 @@ if [ $status -eq 0 ]
     
     if [ -f $folder/../monitoring/activated ]
     then
-      mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; INSERT INTO cluster_log.errors_timeserie (cluster_id,state,error_log,error_type,nodes,created_on_m) VALUES ('$2_${date}','creation','$folder/logs/create_$2_${date}.log','$ERROR_MSG $inst_pool_work_request_error_messages $cn_work_request_error_messages','$1','$end_timestamp');" >> $folder/logs/create_$2_${date}.log 2>&1
+      mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; INSERT INTO cluster_log.errors_timeserie (cluster_id,state,error_log,error_type,nodes,created_on_m,class_name) VALUES ('$2_${date}','creation','$folder/logs/create_$2_${date}.log','$ERROR_MSG $inst_pool_work_request_error_messages $cn_work_request_error_messages','$1','$end_timestamp','$4');" >> $folder/logs/create_$2_${date}.log 2>&1
       mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; UPDATE cluster_log.clusters SET state='deleting',creation_error='`tail $folder/logs/create_$2_${date}.log | grep Error`' WHERE id='$2_${date}';" >> $folder/logs/create_$2_${date}.log 2>&1
     fi
     rm currently_building
