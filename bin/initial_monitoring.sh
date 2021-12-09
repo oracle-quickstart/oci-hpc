b57dc4a2c7e8aa96c1731e3ddfdaa7d14d7cfe00
arnaudfroidmont <arnaudfroidmont@gmail.com>
2021-12-09 23:35:25 +0100

    Move script to bin folder

--- bin/initial_monitoring.sh
+++ bin/initial_monitoring.sh
@@ -6,7 +6,11 @@ folder=`dirname $scripts`
 end_timestamp=`date -u +'%F %T'`
 bastionName=`hostname`
 cluster_name=${bastionName/-bastion/}
-if [ -f $folder/../monitoring/activated ]
+
+autoscaling_folder=$folder/../autoscaling
+monitoring_folder=$folder/../monitoring
+
+if [ -f $monitoring_folder/activated ]
 then
   ocid=`cat /tmp/initial.mon | grep "cluster_ocid =" | awk '{print $3}'`
   ips=`cat /tmp/initial.mon | grep "private_ips =" | awk '{print $3}'`
@@ -26,7 +30,7 @@ then
   for hostname in ${hostnames}; do
     hostname_array+=( $hostname )
   done
-  source $folder/../monitoring/env
+  source $monitoring_folder/env
   mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; INSERT INTO cluster_log.clusters (id,nodes,class_name,shape,CN,cpu_per_node,cluster_name,state,created,cluster_OCID) VALUES ('$cluster_name','${#ip_array[@]}','$queue','$shape',$cluster_network,36,'$cluster_name','running','$end_timestamp','$ocid');"
   for index in "${!ip_array[@]}"; do
       mysqlsh $ENV_MYSQL_USER@$ENV_MYSQL_HOST -p$ENV_MYSQL_PASS --sql -e "use $ENV_MYSQL_DATABASE_NAME; INSERT INTO cluster_log.nodes (cluster_id,cluster_index,cpus,created,state,class_name,shape,hostname,ip,node_OCID) VALUES ('$cluster_name',$index,36,'$end_timestamp','running','$queue','$shape','${hostname_array[$index]}','${ip_array[$index]}','${ocid_array[$index]}');" 
--- bin/slurm_config.sh
+++ bin/slurm_config.sh
@@ -6,9 +6,10 @@
 
 scripts=`realpath $0`
 folder=`dirname $scripts`
+autoscaling_folder=$folder/../autoscaling/
 playbooks_path=$folder/../playbooks/
 
-if [[ `cat $folder/queues.conf | grep instance_keyword |  uniq -c -d | wc -l ` == 0 ]];
+if [[ `cat $autoscaling_folder/queues.conf | grep instance_keyword |  uniq -c -d | wc -l ` == 0 ]];
 then
    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config.yml
 else
