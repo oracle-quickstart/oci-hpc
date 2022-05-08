73a579581bed6d2d2e5fd54774bc3f488e82f0ff
pvaldria <pvaldria@yahoo.com>
2022-05-07 23:59:54 -0700

    add openldap users and group yml, rename files, pcie tuning, remove topo file reference

--- samples/gpu/nccl_run_alltoall.sh
+++ samples/gpu/nccl_run_alltoall.sh
@@ -6,8 +6,6 @@ set -e
 #     ./nccl_run_alltoall.sh  1  /home/opc/ordered_hostfile_system_name 16 
 
 
-
-
 # number of times to run the nccl test to stress the GPUs and RDMA network. This is different from -n iterations parameter of nccl allreduce which is set below using $iter
 max=$1
 
@@ -45,23 +43,18 @@ do
     source /usr/mpi/gcc/openmpi-4.0.3rc4/bin/mpivars.sh
   fi
 
-
 first_node=`head $hostfile -n 1`
 shape=`ssh $first_node 'curl -sH "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/' | jq .shape`
-
 if [ $shape == \"BM.GPU.B4.8\" ]
 then
   var_UCX_NET_DEVICES=mlx5_0:1
-  var_NCCL_TOPO_FILE=/home/opc/topo-flattened-b4.xml
   var_NCCL_IB_HCA="=mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_14,mlx5_15,mlx5_16,mlx5_17,mlx5_9,mlx5_10,mlx5_11,mlx5_12"
 elif [ $shape == \"BM.GPU4.8\" ]
 then
   var_UCX_NET_DEVICES=mlx5_4:1
-  var_NCCL_TOPO_FILE=/home/opc/topo-flattened.xml
-  var_NCCL_IB_HCA="=mlx5_0,mlx5_2,mlx5_6,mlx5_8,mlx5_10,mlx5_12,mlx5_14,mlx5_16"
+  var_NCCL_IB_HCA="=mlx5_0,mlx5_2,mlx5_6,mlx5_8,mlx5_10,mlx5_12,mlx5_14,mlx5_16,mlx5_1,mlx5_3,mlx5_7,mlx5_9,mlx5_11,mlx5_13,mlx5_15,mlx5_17"
 fi
 
-  # Use NCCL_IB_QPS_PER_CONNECTION=4 for QFAB1.0, should get around 15GB/s NCCL Bus BW. 
   # Use  -x NCCL_MAX_P2P_NCHANNELS=16 until NCCL 2.12 release which has a fix to allow NCCL_MAX_P2P_NCHANNELS=32 for nodes with 16 RDMA NICss
   # final version
   mpirun --mca pml ucx \
@@ -77,7 +70,6 @@ fi
   -x coll_hcoll_enable=0 \
   -x NCCL_IB_GID_INDEX=3 \
   -x NCCL_ALGO=Ring \
-  -x NCCL_TOPO_FILE=${var_NCCL_TOPO_FILE} \
   -x NCCL_IB_HCA="${var_NCCL_IB_HCA}" \
   --np $np --hostfile $hostfile  -N 8 /home/opc/nccl-tests/build/alltoall_perf  -f 2 -g 1 -c 0 -n $iter  >> $logfile
 
