2fbdf619ec0b1f445efc5d5edf553c325e5d942e
arnaudfroidmont <arnaudfroidmont@gmail.com>
2022-05-17 12:04:46 -0600

    Add Backup node in the outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,6 @@ output "private_ips" {
   value = join(" ", local.cluster_instances_ips)
 }
 
+output "backup" {
+  value = var.slurm_ha ? oci_core_instance.backup[0].public_ip : "No Slurm Backup Defined"
+}
