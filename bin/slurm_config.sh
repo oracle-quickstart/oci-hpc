787014e09567842636a4ddc0672d0c7b0081405b
arnaudfroidmont <arnaudfroidmont@gmail.com>
2022-05-25 12:15:35 -0600

    Add an option to start the topology from scratch.

--- bin/slurm_config.sh
+++ bin/slurm_config.sh
@@ -2,6 +2,7 @@
 #
 # Regenerate Slurm Config
 #
+# Add --initial as argument if you need to restart slurm from scratch (Removes the current topology file)
 
 
 scripts=`realpath $0`
@@ -12,7 +13,12 @@ playbooks_path=$folder/../playbooks/
 
 if [[ `cat $conf_folder/queues.conf | grep instance_keyword |  uniq -c -d | wc -l ` == 0 ]];
 then
+   if [ ${@: -1} == "--INITIAL" or ${@: -1} == "--initial" or ${@: -1} == "-INITIAL" or ${@: -1} == "-initial"]
+   then
+      sudo rm /etc/slurm/topology.conf
+      sudo /usr/sbin/slurmctld -c
+   fi
    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config.yml
-else
+ else
    echo "There are some duplicates instance_keyword lines, please make them unique"   
 fi
