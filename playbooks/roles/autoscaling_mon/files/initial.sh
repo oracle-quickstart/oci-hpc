08cf891cd6969210dd3d3cc7f3032fb615d53092
arnaudfroidmont <arnaudfroidmont@gmail.com>
2022-06-10 10:37:47 -0600

    Edit initial.sh file for mysql connector python

--- playbooks/roles/autoscaling_mon/files/initial.sh
+++ playbooks/roles/autoscaling_mon/files/initial.sh
@@ -2,6 +2,7 @@ wget https://dl.grafana.com/oss/release/grafana-7.5.0-1.x86_64.rpm
 sudo yum install -y grafana-7.5.0-1.x86_64.rpm
 sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo yum install -y mysql-shell
+sudo pip3 install protobuf==3.19.4
 sudo pip3 install mysql-connector-python
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
