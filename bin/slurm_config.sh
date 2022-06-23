45375f9dbb721fdecf0daa6fb4a1c754ad7b9565
arnaudfroidmont <arnaudfroidmont@gmail.com>
2022-06-23 15:22:48 -0600

    Reconfigure all clusters if slurn has topo issues

--- bin/slurm_config.sh
+++ bin/slurm_config.sh
@@ -19,6 +19,18 @@ then
       sudo /usr/sbin/slurmctld -c
    fi
    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config.yml
+   if [[ ${@: -1} == "--INITIAL" || ${@: -1} == "--initial" || ${@: -1} == "-INITIAL" || ${@: -1} == "-initial" ]]
+   then
+      for inventory in /opt/oci-hpc/autoscaling/clusters/*/inventory ; 
+      do
+         if [ -f $(dirname $inventory)/currently* ]
+         then
+            echo "Cluster is not in running state"
+         else
+            ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbooks_path/slurm_config_as.yml -i $inventory
+         fi
+      done
+   fi
  else
    echo "There are some duplicates instance_keyword lines, please make them unique"   
 fi
