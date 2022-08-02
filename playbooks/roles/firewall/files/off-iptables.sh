595d7b5b055e2af9c8e7aa17bfc7d886018c5a42
pvaldria <pvaldria@yahoo.com>
2022-08-02 09:51:26 -0700

    off iptables restrictions - allow all

---
+++ playbooks/roles/firewall/files/off-iptables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Turning off the Firewall..."
+which apt-get &> /dev/null
+if [ $? -eq 0 ] ; then
+    echo "" > /etc/iptables/rules.v4
+    echo "" > /etc/iptables/rules.v6
+
+    iptables -F
+    iptables -X
+    iptables -t nat -F
+    iptables -t nat -X
+    iptables -t mangle -F
+    iptables -t mangle -X
+    iptables -P INPUT ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+else
+    service firewalld stop
+    chkconfig firewalld off
+fi
