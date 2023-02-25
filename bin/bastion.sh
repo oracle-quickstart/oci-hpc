e9ffd64e4f857c1d96217073ce6c70951993834d
arnaudfroidmont <arnaudfroidmont@gmail.com>
2023-02-24 22:43:33 -0700

    Fix the terraform install for Ubuntu

--- bin/bastion.sh
+++ bin/bastion.sh
@@ -33,11 +33,6 @@ if [ $ID == "ol" ] || [ $ID == "centos" ] ; then
   sudo yum install -y terraform
 
 elif [ $ID == "debian" ] || [ $ID == "ubuntu" ] ; then 
-  if [ $ID == "debian" ] && [ $VERSION_ID == "9" ] ; then 
-    echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | sudo tee -a /etc/apt/sources.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  fi 
-  
   # checking here as well to be sure that the lock file is not being held
   function fix_apt {
     apt_process=`ps aux | grep "apt update" | grep -v grep | wc -l`
@@ -51,6 +46,14 @@ elif [ $ID == "debian" ] || [ $ID == "ubuntu" ] ; then
         apt_process=$(( apt_process -1 ))
       done
   }
+  fix_apt
+
+  if [ $ID == "debian" ] && [ $VERSION_ID == "9" ] ; then 
+    echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | sudo tee -a /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  fi 
+
+
   sudo sed -i 's/"1"/"0"/g' /etc/apt/apt.conf.d/20auto-upgrades
   sudo apt purge -y --auto-remove unattended-upgrades
   sudo systemctl disable apt-daily-upgrade.timer
@@ -70,23 +73,6 @@ elif [ $ID == "debian" ] || [ $ID == "ubuntu" ] ; then
 
   fix_apt
 
-
-  wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-  
-  sudo apt-get -y install terraform
-  output=$?
-  if [ $output -ne 0 ]
-  then
-      fix_apt
-      sudo apt-get -y install terraform
-  fi
-  fix_apt
   sudo apt-get -y install ansible 
   output=$?
   if [ $output -ne 0 ]
@@ -113,6 +99,31 @@ elif [ $ID == "debian" ] || [ $ID == "ubuntu" ] ; then
   # install oci module
   pip install oci
 
+  wget -O- https://apt.releases.hashicorp.com/gpg | \
+  gpg --dearmor | \
+  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+  
+  sudo apt update && sudo apt install terraform
+  output=$?
+  if [ $output -ne 0 ]
+  then
+      fix_apt  
+      echo "Terraform second try"
+      wget -O- https://apt.releases.hashicorp.com/gpg | \
+      gpg --dearmor | \
+      sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+      echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+      https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+      sudo tee /etc/apt/sources.list.d/hashicorp.list
+  
+      sudo apt update && sudo apt install terraform
+  fi
+  fix_apt
 fi 
 
 ansible-galaxy collection install ansible.netcommon:=2.5.1 --force > /dev/null
