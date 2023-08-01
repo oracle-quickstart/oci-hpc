aeb728013d55783e817ec23753911f1abea8553e
anoopna <anoopks1207@gmail.com>
2023-08-01 23:13:33 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -34,7 +34,7 @@ resource "oci_core_volume_backup_policy" "bastion_boot_volume_backup_policy" {
 resource "oci_core_volume_backup_policy_assignment" "boot_volume_backup_policy" {
   count = var.bastion_boot_volume_backup ? 1 : 0
   asset_id  = oci_core_instance.bastion.boot_volume_id
-  policy_id = oci_core_volume_backup_policy_assignment.bastion_boot_volume_backup_policy.id
+  policy_id = oci_core_volume_backup_policy.bastion_boot_volume_backup_policy.id
 }
 
 resource "oci_resourcemanager_private_endpoint" "rms_private_endpoint" {
