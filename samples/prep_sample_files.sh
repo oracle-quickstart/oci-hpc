be5f1e7df8fd09fe76e580d3aaebc9467419fda0
arnaudfroidmont <arnaudfroidmont@gmail.com>
2023-09-29 13:31:18 -0600

    Change the prep sample files to go to current home

--- samples/prep_sample_files.sh
+++ samples/prep_sample_files.sh
@@ -7,7 +7,7 @@ do
   sudo chmod +x $directory/*.sh
 done;
 
-cp nccl_compile/compile.sh /home/opc/
-cp gpu/*.sbatch /home/opc/
-cp /opt/oci-hpc/bin/node_ordering_by_rack.py /home/opc/
+cp nccl_compile/compile.sh ~
+cp gpu/*.sbatch ~
+cp /opt/oci-hpc/bin/node_ordering_by_rack.py ~
 
