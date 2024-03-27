c73c8801b6c6af2cd96d5d845a7eebd24f67b570
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2024-03-27 11:49:14 -0600

    Make healthchecks check valid for A100

--- playbooks/roles/slurm/files/healthchecks.sh
+++ playbooks/roles/slurm/files/healthchecks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 shape=`curl -sH "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq .shape`
-if [ "${shape}" = \"BM.GPU.H100.8\" ]
+if [ "${shape}" = \"BM.GPU.H100.8\" ] || [ "${shape}" == \"BM.GPU.A100-v2.8\" ] || [ "${shape}" == \"BM.GPU4.8\" ] || [ "${shape}" == \"BM.GPU.B4.8\" ]
 then
-    sudo python3 /opt/oci-hpc/healthchecks/check_h100_setup.py --slurm > /tmp/latest_healthcheck.log 2>&1
+    sudo python3 /opt/oci-hpc/healthchecks/check_gpu_setup.py --slurm > /tmp/latest_healthcheck.log 2>&1
     DRAIN_MSG=`cat /tmp/latest_healthcheck.log | grep "Healthcheck::"`
     if [ "$DRAIN_MSG" != "" ]
     then
