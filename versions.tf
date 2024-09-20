6db3381ed2a4f091174c564ee402f0e466112af1
Marcin Zablocki <marcin.zablocki@oracle.com>
2024-09-20 15:46:21 -0700

    Update version constraints for providers

--- versions.tf
+++ versions.tf
@@ -3,7 +3,19 @@ terraform {
   required_providers {
      oci = {
          source = "oracle/oci"
-         version = "6.9.0"
+         version = ">= 6.9.0"
+     }
+     local = { 
+           source = "hashicorp/local"
+           version = ">= 2.1.0"
+     }
+     tls = {
+           source = "hashicorp/tls"
+           version = ">= 3.0.0"
+     }
+     random = {
+           source = "hashicorp/random"
+           version = ">= 3.0.0"
      }
   }
 }
