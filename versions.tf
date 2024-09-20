18c573a89f80c72d375f518d72c4c7f46fcef7d2
Marcin Zablocki <marcin.zablocki@oracle.com>
2024-09-20 15:51:26 -0700

    Add null provider to version constraints

--- versions.tf
+++ versions.tf
@@ -17,5 +17,9 @@ terraform {
            source = "hashicorp/random"
            version = ">= 3.0.0"
      }
+     null = {    
+           source = "hashicorp/null"
+           version = ">= 3.0.0"
+     }
   }
 }
