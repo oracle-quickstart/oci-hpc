9548a66c6c5071090808ddef643615da34f3482b
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-01-07 12:11:25 -0700

    Add hostname_convention

--- compute-nodes.tf
+++ compute-nodes.tf
@@ -64,6 +64,7 @@ resource "oci_core_instance" "compute_cluster_instances" {
     "cluster_name"   = local.cluster_name
     "parent_cluster" = local.cluster_name
     "controller_name" = oci_core_instance.controller.display_name
+    "hostname_convention" = var.hostname_convention
   }
 
   metadata = {
