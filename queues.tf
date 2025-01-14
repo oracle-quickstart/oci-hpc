214ca5f820a4a5926855bf84fdc6c7f14336fd46
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-01-14 13:53:43 -0700

    Change defualt visibility and retention periods of the queue

--- queues.tf
+++ queues.tf
@@ -1,4 +1,6 @@
 resource "oci_queue_queue" "queue" {
   display_name   = "${local.cluster_name}-queue"
   compartment_id = var.targetCompartment
+  visibility_in_seconds = 120
+  retention_in_seconds = 172800
 }
