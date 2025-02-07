4f43bfaf5c0a668dba4dc46df44f5c6337824fac
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-02-06 23:32:54 -0700

    fix oci tf version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">= 6.25.0"
+      version = ">= 6.23.0"
     }
     local = {
       source  = "hashicorp/local"
