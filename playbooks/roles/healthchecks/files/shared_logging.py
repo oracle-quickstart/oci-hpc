6e640a447dc48b00ad97d755e6b77f520a6bda2c
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-04-01 16:15:25 -0600

    Make sure healthcheck is run by root to add to file

--- playbooks/roles/healthchecks/files/shared_logging.py
+++ playbooks/roles/healthchecks/files/shared_logging.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import logging
+import os
 logging.basicConfig(level="INFO", format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('nhc')
 
-file_handler = logging.FileHandler("/tmp/latest_healthcheck.log",mode='w')
-logger.addHandler(file_handler)
+
+if os.geteuid() == 0:
+    file_handler = logging.FileHandler("/tmp/latest_healthcheck.log",mode='w')
+    logger.addHandler(file_handler)
