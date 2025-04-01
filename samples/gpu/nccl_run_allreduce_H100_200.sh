f82152d77f57c219e132e6a09d9ae2da9bccdbb1
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-04-01 14:22:45 -0600

    Add H200 support

--- samples/gpu/nccl_run_allreduce_H100_200.sh
+++ samples/gpu/nccl_run_allreduce_H100_200.sh
@@ -42,22 +42,28 @@ do
       echo "Could not find MPIPATH"; exit; fi
 
   source $mpivars_path
-
+  hpcx_path=`ls /opt/hpcx-*/nccl_rdma_sharp_plugin/lib/libnccl-net.so`
+  if [[ "$hpcx_path" == "" ]]; then
+      hpcx_path=none
+  fi
   first_node=`head $hostfile -n 1`
   shape=`ssh $first_node 'curl -sH "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/' | jq .shape`
   if [ $shape == \"BM.GPU.H100.8\" ]
   then
     var_UCX_NET_DEVICES=eth0
     var_NCCL_IB_HCA="=mlx5_0,mlx5_1,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_9,mlx5_10,mlx5_12,mlx5_13,mlx5_14,mlx5_15,mlx5_16,mlx5_17"
+  elif [ $shape == \"BM.GPU.H200.8\" ]
+  then
+    var_UCX_NET_DEVICES=eth0
+    var_NCCL_IB_HCA="=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11"  
   else
-    echo "Use the appropriate nccl test run script for non H100 nodes"
+    echo "Use the appropriate nccl test run script for non H100/H200 nodes"
   fi
 
   mpirun --mca pml ucx \
   --bind-to numa \
   -npernode 8 \
   --mca coll ^hcoll \
-  -x NCCL_CROSS_NIC=2 \
   -x NCCL_DEBUG=WARN \
   -x NCCL_CUMEM_ENABLE=0 \
   -x NCCL_IB_SPLIT_DATA_ON_QPS=0 \
@@ -66,18 +72,16 @@ do
   -x NCCL_IB_TC=41 \
   -x NCCL_IB_SL=0 \
   -x NCCL_IB_TIMEOUT=22 \
-  -x NCCL_NET_PLUGIN=none \
+  -x NCCL_NET_PLUGIN=${hpcx_path} \
   -x HCOLL_ENABLE_MCAST_ALL=0 \
   -x coll_hcoll_enable=0 \
   -x UCX_TLS=tcp \
   -x UCX_NET_DEVICES=${var_UCX_NET_DEVICES} \
   -x RX_QUEUE_LEN=8192 \
   -x IB_RX_QUEUE_LEN=8192 \
-  -x NCCL_BUFFSIZE=16777216 \
   -x NCCL_SOCKET_IFNAME=${var_UCX_NET_DEVICES} \
   -x NCCL_IGNORE_CPU_AFFINITY=1 \
   -x NCCL_IB_HCA="${var_NCCL_IB_HCA}" \
-  -x NCCL_TOPO_FILE=~/H100-topology.xml \
   --np $np --hostfile $hostfile  /opt/oci-hpc/nccl-test/build/all_reduce_perf -b 1G -e 16G -f 2 -g 1 >>  $logfile
 
   tail -n 32 $logfile
