917ad65f1cf8de6f8350cc65effba65fb969b226
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-05-16 00:37:35 -0600

    Ansible syntax

--- playbooks/roles/slurm/files/resume.sh
+++ playbooks/roles/slurm/files/resume.sh
@@ -23,11 +23,11 @@ for cluster in "${!cluster_hosts[@]}"; do
     instance_type=${cluster_types[$cluster]}
     count=$(echo "$host_list" | tr ',' '\n' | wc -l)
     echo $host_list $instance_type $count
-    if /config/venv/bin/python3 /config/mgmt/manage.py clusters list --json | jq -e ".[] | select(. == \"$cluster\")" > /dev/null; then
+    if /opt/oci-hpc/venv/bin/python3 /config/mgmt/manage.py clusters list --json | jq -e ".[] | select(. == \"$cluster\")" > /dev/null; then
         echo "Adding nodes to existing cluster: $cluster"
-        /config/venv/bin/python3 /config/mgmt/manage.py clusters add --cluster "$cluster" --count "$count" --names "$host_list"
+        /opt/oci-hpc/venv/bin/python3 /config/mgmt/manage.py clusters add --cluster "$cluster" --count "$count" --names "$host_list"
     else
         echo "Creating new cluster: $cluster"
-        /config/venv/bin/python3 /config/mgmt/manage.py clusters create --cluster "$cluster" --count "$count" --instancetype "$instance_type" --names "$host_list"
+        /opt/oci-hpc/venv/bin/python3 /config/mgmt/manage.py clusters create --cluster "$cluster" --count "$count" --instancetype "$instance_type" --names "$host_list"
     fi
 done
--- playbooks/roles/slurm/files/suspend.sh
+++ playbooks/roles/slurm/files/suspend.sh
@@ -5,5 +5,5 @@ hosts=$(scontrol show hostnames "$1")
 
 for host in $hosts
 do
-    /config/venv/bin/python3 /config/mgmt/manage.py nodes terminate --nodes "$host"
+    /opt/oci-hpc/venv/bin/python3 /config/mgmt/manage.py nodes terminate --nodes "$host"
 done
