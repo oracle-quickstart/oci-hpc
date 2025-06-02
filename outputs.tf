87e216a921cff7ac190769ac7edd16c668fd0987
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-06-02 11:02:21 -0600

    Remove fabrics output

--- outputs.tf
+++ outputs.tf
@@ -17,13 +17,3 @@ output "login" {
 output "monitoring" {
   value = var.monitoring_node ? local.host_monitoring : "No Monitoring Node Defined"
 }
-
-output "fabric" {
-  value = join(
-    ",",
-    flatten([
-      for fabric in data.oci_core_compute_gpu_memory_fabrics.test_compute_gpu_memory_fabrics.compute_gpu_memory_fabric_collection :
-      [for item in fabric.items : item.id]
-    ])
-  )
-}
