1882ba4e2f99a2b8820507ea946932089f3ef37b
bruno-garbaccio <bruno.garbaccio@oracle.com>
2025-09-18 09:16:29 +0200

    fixing v7.17 in versions
    setting to v.7.17 due to a bug in 7.18 for FSS sept.12 2025 for provider oracle/oci

--- .github/workflows/provider_testing.tf
+++ .github/workflows/provider_testing.tf
@@ -1,15 +1,3 @@
-#setting to v.7.17 due to a bug in 7.18 for FSS sept.12 2025
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "7.17.0"
-    }
-  }
-}
-
-
-
 provider "oci" {
 auth = "InstancePrincipal"
 region           = "${var.region}"
