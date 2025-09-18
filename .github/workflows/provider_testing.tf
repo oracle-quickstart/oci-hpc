e774850d4489f0c6c4be2c5b4c87ce549d93609b
bruno-garbaccio <bruno.garbaccio@oracle.com>
2025-09-18 09:10:59 +0200

    Update provider_testing.tf

--- .github/workflows/provider_testing.tf
+++ .github/workflows/provider_testing.tf
@@ -1,8 +1,18 @@
 #setting to v.7.17 due to a bug in 7.18 for FSS sept.12 2025
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "7.17.0"
+    }
+  }
+}
+
+
+
 provider "oci" {
 auth = "InstancePrincipal"
 region           = "${var.region}"
-version = "7.17.0" 
 }
 
 # provider for home region for auth token provisioning
