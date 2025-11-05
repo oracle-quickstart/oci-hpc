4a7eecf32abbc5eb008825bd8ed4d33d427bd64a
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2025-11-04 23:03:22 -0700

    Change oci provider default version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">= 6.21.0"
+      version = ">= 7.19.0"
     }
     local = {
       source  = "hashicorp/local"
