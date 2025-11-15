98665a98aa7ba63da5e05473f2bc5851077a37c1
Nuruddin Ahmed <nuruddin.ahmed@oracle.com>
2025-11-14 23:47:55 -0600

    Fix permission for ansible tmp

--- bin/controller.sh
+++ bin/controller.sh
@@ -3,134 +3,135 @@
 # Cluster init configuration script
 #
 
+# Re-exec as root if not already root (so Ansible + install all run as root)
+if [ "$(id -u)" -ne 0 ]; then
+  exec sudo -E /bin/bash "$0" "$@"
+fi
+
 #
 # wait for cloud-init completion on the controller host
 #
-
 execution=1
 
 ssh_options="-i ~/.ssh/cluster.key -o StrictHostKeyChecking=no"
 sudo cloud-init status --wait
 
 source /etc/os-release
-
-
 if [ ! -d /opt/oci-hpc ] ; then
   sudo mkdir /opt/oci-hpc
-fi
-if [ "$ID" = "ubuntu" ] ; then
+fi   
+if [ $ID == "ubuntu" ] ; then
   sudo chown -R ubuntu:ubuntu /opt/
 else
   sudo chown -R opc:opc /opt/
 fi
 
-
-vid=$(echo "$VERSION" | awk -F. '{print $1}')
-if [ "$ID" = "ol" ] ; then
-  if [ "$vid" = "7" ] ; then
+vid=`echo $VERSION|awk -F. '{print $1}'`
+if [ $ID == "ol" ] ; then
+  if [ $vid == 7 ] ; then
     repo="ol7_developer_EPEL"
-    sudo osms unregister
-  elif [ "$vid" = "8" ] ; then
+    sudo osms unregister 
+  elif [ $vid == 8 ] ; then
     repo="ol8_developer_EPEL"
     if command -v osms >/dev/null 2>&1; then
-      sudo osms unregister
+      sudo osms unregister 
     fi
-  elif [ "$vid" = "9" ] ; then
+  elif [ $vid == 9 ] ; then
     repo="ol9_developer_EPEL"
   fi
-elif [ "$ID" = "centos" ] ; then
+elif [ $ID == "centos" ] ; then
   repo="epel"
 fi
 
-
 export UV_INSTALL_DIR=/config/venv/${ID^}_${VERSION_ID}_$(uname -m)/
 export VENV_PATH=${UV_INSTALL_DIR}/oci
-
 if [ ! -d "${UV_INSTALL_DIR}" ]; then
   export creating_python_install_dir=true
-
   export UV_CACHE_DIR=${UV_INSTALL_DIR}/cache
   export UV_PYTHON_INSTALL_DIR=${UV_INSTALL_DIR}/python
   export UV_LOCAL_CACHE_DIR=/opt/uv_local/cache/
   export UV_LOCAL_PYTHON_INSTALL_DIR=${UV_LOCAL_CACHE_DIR}/python
 
-  sudo mkdir -p "${UV_LOCAL_CACHE_DIR}" "${UV_CACHE_DIR}"
-  sudo mkdir -p "${UV_LOCAL_PYTHON_INSTALL_DIR}" "${UV_PYTHON_INSTALL_DIR}"
-  if [ "$ID" = "ubuntu" ] ; then
-    sudo chown -R ubuntu:ubuntu "${UV_LOCAL_CACHE_DIR}" "${UV_INSTALL_DIR}"
+  sudo mkdir -p ${UV_LOCAL_CACHE_DIR}
+  sudo mkdir -p ${UV_CACHE_DIR}:${UV_LOCAL_CACHE_DIR}
+  sudo mkdir -p ${UV_PYTHON_INSTALL_DIR}:${UV_LOCAL_PYTHON_INSTALL_DIR}
+  if [ $ID == "ubuntu" ] ; then
+    sudo chown -R ubuntu:ubuntu ${UV_LOCAL_CACHE_DIR}
+    sudo chown -R ubuntu:ubuntu ${UV_INSTALL_DIR}
   else
-    sudo chown -R opc:opc "${UV_LOCAL_CACHE_DIR}" "${UV_INSTALL_DIR}"
+    sudo chown -R opc:opc ${UV_LOCAL_CACHE_DIR}
+    sudo chown -R opc:opc ${UV_INSTALL_DIR}
   fi
 fi
 
-
 # Install ansible and other required packages
-if [ "$ID" = "ol" ] || [ "$ID" = "centos" ] ; then
-  if [ "$vid" = "7" ]; then
+if [ $ID == "ol" ] || [ $ID == "centos" ] ; then 
+  if [ $vid == 7 ]; then
     sudo yum-config-manager --save --setopt=ol7_oci_included.skip_if_unavailable=true
-    sudo yum makecache --enablerepo="$repo"
-    sudo yum install --enablerepo="$repo" -y ansible python-netaddr python-dnf
-  elif [ "$vid" = "8" ] || [ "$vid" = "9" ] ; then
-    sudo def install -y python3 python3-pip python3-dnf java-11-openjdk-headless http-parser
+    sudo yum makecache --enablerepo=$repo
+    sudo yum install --enablerepo=$repo -y ansible python-netaddr python-dnf
+  elif [ $vid == 8 ] || [ $vid == 9 ] ; then
+    sudo dnf install -y python3 python3-pip python3-dnf java-11-openjdk-headless http-parser
   fi
 
-elif [ "$ID" = "debian" ] || [ "$ID" = "ubuntu" ] ; then
-
+elif [ $ID == "debian" ] || [ $ID == "ubuntu" ] ; then 
   # checking here as well to be sure that the lock file is not being held
-  fix_apt() {
-    apt_process=$(ps aux | grep "apt update" | grep -v grep | wc -l)
-    apt_process=$(( apt_process - 1 ))
-    while [ "$apt_process" -ge 1 ]; do
-      echo "wait until apt update is done"
-      sleep 10s
-      ps aux | grep "apt update" | grep -v grep || true
-      apt_process=$(ps aux | grep "apt update" | grep -v grep | wc -l)
-      apt_process=$(( apt_process - 1 ))
-    done
+  function fix_apt {
+    apt_process=`ps aux | grep "apt update" | grep -v grep | wc -l`
+    apt_process=$(( apt_process -1 ))
+    while [ $apt_process -ge 1 ]
+      do
+        echo "wait until apt update is done"
+        sleep 10s
+        ps aux | grep "apt update" | grep -v grep
+        apt_process=`ps aux | grep "apt update" | grep -v grep | wc -l`
+        apt_process=$(( apt_process -1 ))
+      done
   }
-
   fix_apt
 
-  if [ "$ID" = "debian" ] && [ "$VERSION_ID" = "9" ] ; then
-    echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
+  if [ $ID == "debian" ] && [ $VERSION_ID == "9" ] ; then 
+    echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | sudo tee -a /etc/apt/sources.list
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  fi
+  fi 
 
-
-  sudo sed -i 's/"1"/"0"/g' /etc/apt/apt.conf.d/20auto-upgrades || true
-  sudo apt purge -y --auto-remove unattended-upgrades || true
-  sudo systemctl stop apt-daily-upgrade.timer apt-daily.timer 2>/dev/null || true
-  sudo systemctl disable apt-daily-upgrade.timer apt-daily.timer 2>/dev/null || true
-  sudo systemctl mask apt-daily-upgrade.service apt-daily.service 2>/dev/null || true
+  sudo sed -i 's/"1"/"0"/g' /etc/apt/apt.conf.d/20auto-upgrades
+  sudo apt purge -y --auto-remove unattended-upgrades
+  sudo systemctl stop apt-daily-upgrade.timer
+  sudo systemctl disable apt-daily-upgrade.timer
+  sudo systemctl mask apt-daily-upgrade.service
+  sudo systemctl stop apt-daily.timer
+  sudo systemctl disable apt-daily.timer
+  sudo systemctl mask apt-daily.service
 
   sleep 10s
 
-  sudo apt-mark hold linux-oracle linux-headers-oracle linux-image-oracle 2>/dev/null || true
+  sudo apt-mark hold linux-oracle linux-headers-oracle linux-image-oracle
 
   fix_apt
   sleep 10s
-  sudo apt -y --fix-broken install || true
+  sudo apt -y --fix-broken install
 
   fix_apt
   sudo apt update
-  if [ "$ID" = "ubuntu" ] && [ "$VERSION_ID" = "20.04" ] ; then
+  if [ $ID == "ubuntu" ] && [ $VERSION_ID == "20.04" ] ; then
     sudo apt-get -y install python3 python3-pip jq openjdk-11-jre-headless libhttp-parser2.9
   else
-    sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf 2>/dev/null || true
-
+    sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
     apt_success=1
-    while [ "$apt_success" -ge 1 ]; do
-      echo "Installing base packages (python3, pip, jq, openjdk, http-parser)..."
-      sleep 10s
-      sudo apt-get -y install python3 python3-pip jq openjdk-11-jre-headless libhttp-parser2.9
-      apt_success=$?
-      echo "apt-get exit: $apt_success"
-    done
+    while [ $apt_success -ge 1 ]
+      do
+        echo "wait until apt update is done"
+        sleep 10s
+        sudo apt-get -y install python3 python3-pip jq openjdk-11-jre-headless libhttp-parser2.9
+        apt_success=$?
+        echo $apt_success
+      done
   fi
-  if [ "$ID" = "ubuntu" ] && [ "$VERSION_ID" = "20.04" ] ; then
+  if [ $ID == "ubuntu" ] && [ $VERSION_ID == "20.04" ] ; then
     fix_apt
     sudo apt-get -y install python3 python3-pip jq openjdk-11-jre-headless libhttp-parser2.9
-  elif [ "$ID" = "ubuntu" ] && [ "$VERSION_ID" = "22.04" ] ; then
+  elif [ $ID == "ubuntu" ] && [ $VERSION_ID == "22.04" ] ; then
     fix_apt
     sudo apt-get -y install python3 python3-pip jq openjdk-11-jre-headless libhttp-parser2.9
   else
@@ -141,30 +142,31 @@ elif [ "$ID" = "debian" ] || [ "$ID" = "ubuntu" ] ; then
   cd /tmp
   LATEST_OCICLI=$(curl -s -L https://api.github.com/repos/oracle/oci-cli/releases/latest | jq -r '.name')
 
-
   # First try to install into /opt/oci-cli
-  bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)" \
-    -s --accept-all-defaults --install-dir /opt/oci-cli --oci-cli-version "$LATEST_OCICLI" 2>&1
+   bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)" \
+      -s --accept-all-defaults --install-dir /opt/oci-cli --oci-cli-version "$LATEST_OCICLI"  2>&1
 fi
 
 
-# install uv
-if [ -n "${creating_python_install_dir:-}" ]; then
+# install uv  
+if [ $creating_python_install_dir ]; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
 
-  source "${UV_INSTALL_DIR}/env"
+  source ${UV_INSTALL_DIR}/env
 
-  if { [ "$ID" = "ol" ] || [ "$ID" = "centos" ]; } && [ "$vid" = "8" ] ; then
-    uv python install 3.10 --force
+  if ( [ $ID == "ol" ] || [ $ID == "centos" ] ) && [ $vid == 8 ] ; then 
+      uv python install 3.10 --force
   else
-    uv python install 3.12 --force
+      uv python install 3.12 --force
   fi
-  uv venv "${UV_INSTALL_DIR}/oci" --clear
-  source "${UV_INSTALL_DIR}/oci/bin/activate"
+  uv venv ${UV_INSTALL_DIR}/oci --clear
+  source ${UV_INSTALL_DIR}/oci/bin/activate
   uv pip install pip
   uv pip install ansible
-  if { [ "$ID" = "ol" ] || [ "$ID" = "centos" ]; } && [ "$vid" = "8" ]; then
-    uv pip install "ansible-core==2.12"
+  if [ $ID == "ol" ] || [ $ID == "centos" ] ; then 
+    if [ $vid == 8 ]; then
+      uv pip install ansible-core==2.12
+    fi
   fi
   uv pip install oci-cli
   uv pip install oci
@@ -237,45 +239,33 @@ if [ -n "${creating_python_install_dir:-}" ]; then
   uv pip install "fastapi[standard-no-fastapi-cloud-cli]"
   uv pip install uvicorn
 
-  "${VENV_PATH}/bin/ansible-galaxy" collection install ansible.netcommon --upgrade --force >/dev/null
-  "${VENV_PATH}/bin/ansible-galaxy" collection install community.general --upgrade --force >/dev/null
-  "${VENV_PATH}/bin/ansible-galaxy" collection install ansible.posix --force >/dev/null
-  "${VENV_PATH}/bin/ansible-galaxy" collection install community.crypto --force >/dev/null
-  "${VENV_PATH}/bin/ansible-galaxy" collection install ansible.utils --force >/dev/null
-  if { [ "$ID" = "ol" ] || [ "$ID" = "centos" ]; } && [ "$vid" = "8" ]; then
-    export REQUESTS_CA_BUNDLE=/etc/pki/tls/certs/ca-bundle.crt
-    export SSL_CERT_FILE=/etc/pki/tls/certs/ca-bundle.crt
-    "${VENV_PATH}/bin/ansible-galaxy" collection install git+https://github.com/oracle/oci-ansible-collection.git >/dev/null
+  $VENV_PATH/bin/ansible-galaxy collection install ansible.netcommon --upgrade --force > /dev/null
+  $VENV_PATH/bin/ansible-galaxy collection install community.general --upgrade --force > /dev/null
+  $VENV_PATH/bin/ansible-galaxy collection install ansible.posix --force > /dev/null
+  $VENV_PATH/bin/ansible-galaxy collection install community.crypto --force > /dev/null
+  $VENV_PATH/bin/ansible-galaxy collection install ansible.utils --force > /dev/null
+  if ( [ $ID == "ol" ] || [ $ID == "centos" ] ) && [ $vid == 8 ] ; then 
+      export REQUESTS_CA_BUNDLE=/etc/pki/tls/certs/ca-bundle.crt
+      export SSL_CERT_FILE=/etc/pki/tls/certs/ca-bundle.crt
+      $VENV_PATH/bin/ansible-galaxy collection install git+https://github.com/oracle/oci-ansible-collection.git > /dev/null
   else
-    "${VENV_PATH}/bin/ansible-galaxy" collection install oracle.oci --force >/dev/null
+      $VENV_PATH/bin/ansible-galaxy collection install oracle.oci --force > /dev/null
   fi
 fi
 
-###############################################################################
-# Ansible configuration (ansible.cfg)
-###############################################################################
-
 threads=$(nproc)
-forks=$((threads * 8))
+forks=$(($threads * 8))
 
 if [ ! -d /etc/ansible ] ; then
   sudo mkdir /etc/ansible
-  if [ "$ID" = "ubuntu" ] ; then
+  if [ $ID == "ubuntu" ] ; then
     sudo chown ubuntu:ubuntu /etc/ansible
   else
     sudo chown opc:opc /etc/ansible
   fi
 fi
 
-# Generate a fresh ansible.cfg from ansible-config
-"${VENV_PATH}/bin/ansible-config" init --disabled -t all | sudo tee /etc/ansible/ansible.cfg >/dev/null
-
-# For OL8 only, comment out ALL remote_tmp lines within [powershell]
-# to avoid "option 'remote_tmp' already exists".
-if [[ "$ID" = "ol" && "$vid" -eq 8 ]]; then
-  sudo sed -i '/^\[powershell\]/,/^\[/ s/^remote_tmp/#remote_tmp/' /etc/ansible/ansible.cfg
-fi
-
+$VENV_PATH/bin/ansible-config init --disabled -t all | sudo tee /etc/ansible/ansible.cfg > /dev/null
 sudo sed -i "s/^\(#\|;\)forks.*/forks = ${forks}/" /etc/ansible/ansible.cfg
 sudo sed -i "s/^\(#\|;\)fact_caching=.*/fact_caching=jsonfile/" /etc/ansible/ansible.cfg
 sudo sed -i "0,/^\(#\|;\)fact_caching_connection.*/s//fact_caching_connection=\/tmp\/ansible/" /etc/ansible/ansible.cfg
@@ -283,14 +273,11 @@ sudo sed -i "s/^\(#\|;\)bin_ansible_callbacks.*/bin_ansible_callbacks=True/" /et
 sudo sed -i "s/^\(#\|;\)retries.*/retries=5/" /etc/ansible/ansible.cfg
 sudo sed -i "s/^\(#\|;\)connect_timeout.*/connect_timeout=300/" /etc/ansible/ansible.cfg
 sudo sed -i "s/^\(#\|;\)command_timeout.*/command_timeout=120/" /etc/ansible/ansible.cfg
-
-# Force remote_tmp under [defaults] (and only there)
 sudo sed -i "/^\[defaults\]/,/^\[/ s/^\(#\|;\)remote_tmp.*/remote_tmp=\/tmp\/.ansible-tmp/" /etc/ansible/ansible.cfg
 
 # Replace the legacy yaml callback with the built-in default and enable YAML output
 sudo sed -i 's/^\([#;]\s*\)\?stdout_callback.*/stdout_callback = default/' /etc/ansible/ansible.cfg
-
-# Ensure [callback_default] section exists and uses YAML output
+# Ensure the default callback outputs YAML (append the section if not present)
 if ! sudo grep -q '^\[callback_default\]' /etc/ansible/ansible.cfg; then
   sudo tee -a /etc/ansible/ansible.cfg >/dev/null <<'EOF'
 [callback_default]
@@ -300,5 +287,4 @@ else
   # Make sure result_format=yaml is set under [callback_default]
   sudo sed -i '/^\[callback_default\]/,/^\[/{s/^\([#;]\s*\)\?result_format.*/result_format = yaml/}' /etc/ansible/ansible.cfg
 fi
-
 echo "Controller setup complete. VENV_PATH=${VENV_PATH}"
