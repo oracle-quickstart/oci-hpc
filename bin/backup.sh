fc638237ba0028e574aabfc8bd741b86a5961cc7
Omar Awile <omar.awile@oracle.com>
2025-11-27 20:45:50 +0100

    Make all ansible-galaxy installs "upgrade installs" Ensure that all ansible-galaxy collections are installed with the --upgrade flag. We want to be sure we get everywhere an up-to-date version aligned with the ansible version we have. This patch also moves all collection installations out of the if-branch that should be specific for the oci colleciton

--- bin/backup.sh
+++ bin/backup.sh
@@ -130,15 +130,15 @@ export VENV_PATH=${UV_INSTALL_DIR}/oci
 
 $VENV_PATH/bin/ansible-galaxy collection install -c ansible.netcommon --upgrade --force > /dev/null
 $VENV_PATH/bin/ansible-galaxy collection install -c community.general --upgrade --force > /dev/null
-$VENV_PATH/bin/ansible-galaxy collection install -c ansible.posix --force > /dev/null
-$VENV_PATH/bin/ansible-galaxy collection install -c community.crypto --force > /dev/null
-$VENV_PATH/bin/ansible-galaxy collection install -c ansible.utils --force > /dev/null
+$VENV_PATH/bin/ansible-galaxy collection install -c ansible.posix --upgrade --force > /dev/null
+$VENV_PATH/bin/ansible-galaxy collection install -c community.crypto --upgrade --force > /dev/null
+$VENV_PATH/bin/ansible-galaxy collection install -c ansible.utils --upgrade --force > /dev/null
 if ( [ $ID == "ol" ] || [ $ID == "centos" ] ) && [ $vid == 8 ] ; then 
     export REQUESTS_CA_BUNDLE=/etc/pki/tls/certs/ca-bundle.crt
     export SSL_CERT_FILE=/etc/pki/tls/certs/ca-bundle.crt
     $VENV_PATH/bin/ansible-galaxy collection install -c git+https://github.com/oracle/oci-ansible-collection.git > /dev/null
 else
-    $VENV_PATH/bin/ansible-galaxy collection install -c oracle.oci --force > /dev/null
+    $VENV_PATH/bin/ansible-galaxy collection install -c oracle.oci --upgrade --force > /dev/null
 fi
 
 threads=$(nproc)
