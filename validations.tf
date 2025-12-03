7098d4f8f862736da798b14b6d5bbe1838e5e0df
yanncaniouoracle <yann.caniou@gmail.com>
2025-12-03 23:42:31 +0100

    Add shape - image validation step
    In this commit, validation tests are added to check whether the selected image is compatible with the associated shapes. This can be useful for compatible shapes such as BM.GPU.GB200.4.

--- validations.tf
+++ validations.tf
@@ -1,3 +1,5 @@
+# get nodes image IDs
+
 data "oci_core_image" "controller_validation" {
     image_id = local.controller_image
 }
@@ -6,14 +8,14 @@ data "oci_core_image" "compute_validation" {
     image_id = local.compute_image
 }
 
-data "oci_core_image" "login_validation" {
-    count               = var.login_node ? 1 : 0
+# get image compatible shapes
+
+data "oci_core_image_shapes" "test_controller_image_shapes" {
     image_id = local.controller_image
 }
 
-data "oci_core_image" "monitoring_validation" {
-    count               = var.monitoring_node ? 1 : 0
-    image_id = local.controller_image
+data "oci_core_image_shapes" "test_compute_image_shapes" {
+    image_id = local.compute_image
 }
 
 
@@ -23,28 +25,32 @@ locals {
     can(regex("(?i)ubuntu", data.oci_core_image.controller_validation.operating_system)) ? "ubuntu" :
     can(regex("(?i)oracle", data.oci_core_image.controller_validation.operating_system)) ? "opc" :
     "unknown"
-  )  
-  expected_username_login = (
-    can(regex("(?i)ubuntu", data.oci_core_image.login_validation[0].operating_system)) ? "ubuntu" :
-    can(regex("(?i)oracle", data.oci_core_image.login_validation[0].operating_system)) ? "opc" :
-    "unknown"
   )
-  expected_username_monitoring = (
-    can(regex("(?i)ubuntu", data.oci_core_image.monitoring_validation[0].operating_system)) ? "ubuntu" :
-    can(regex("(?i)oracle", data.oci_core_image.monitoring_validation[0].operating_system)) ? "opc" :
-    "unknown"
-  )  
   expected_username_compute = (
     can(regex("(?i)ubuntu", data.oci_core_image.compute_validation.operating_system)) ? "ubuntu" :
     can(regex("(?i)oracle", data.oci_core_image.compute_validation.operating_system)) ? "opc" :
     "unknown"
-  )  
-  
+  )
+  compatible_controller_shapes = [ for element in data.oci_core_image_shapes.test_controller_image_shapes.image_shape_compatibilities : element.shape ]
+  compatible_compute_shapes = [ for element in data.oci_core_image_shapes.test_compute_image_shapes.image_shape_compatibilities : element.shape ]
 }
 
 
-resource "null_resource" "validate_controller_username" {
+#  Validate that FSS is present to host the slurm database
 
+resource "null_resource" "validate_ha_setup" {
+  count = local.invalid_ha_config ? 1 : 0
+  lifecycle {
+    precondition {
+      condition     = !local.invalid_ha_config
+      error_message = "Error: Slurm HA configuration requires a shared NFS. Add NFS or create FSS (create_fss = true). "
+    }
+  }
+}
+
+# validate that the management and compute nodes usernames are set correctly
+
+resource "null_resource" "validate_controller_username" {
   lifecycle {
     precondition {
       condition     = var.controller_username == local.expected_username_controller
@@ -54,7 +60,6 @@ resource "null_resource" "validate_controller_username" {
 }
 
 resource "null_resource" "validate_compute_username" {
-
   lifecycle {
     precondition {
       condition     = var.compute_username == local.expected_username_compute
@@ -63,36 +68,33 @@ resource "null_resource" "validate_compute_username" {
   }
 }
 
-resource "null_resource" "validate_login_username" {
-  count               = var.login_node ? 1 : 0
+# validate that the images are compatible with the node CPU architecture (x86_64 versus aarch64)
+
+resource "null_resource" "validate_controller_compatibility" {
   lifecycle {
     precondition {
-      condition     = var.controller_username == local.expected_username_login
-      error_message = "Invalid username for the selected OS on login node. Use 'ubuntu' for Ubuntu images and 'opc' for Oracle Linux images."
+      condition     = contains(local.compatible_controller_shapes, var.controller_shape)
+      error_message = "Selected controller image is not compatible with the controller shape CPU architecture."
     }
   }
 }
 
-resource "null_resource" "validate_monitoring_username" {
-  count               = var.monitoring_node ? 1 : 0
+resource "null_resource" "validate_cluster_network_compatibility" {
+  count = var.rdma_enabled ? 1 : 0
   lifecycle {
     precondition {
-      condition     = var.controller_username == local.expected_username_monitoring
-      error_message = "Invalid username for the selected OS on monitoring node. Use 'ubuntu' for Ubuntu images and 'opc' for Oracle Linux images."
+      condition     = contains(local.compatible_compute_shapes, var.cluster_network_shape)
+      error_message = "Selected compute image is not compatible with the compute node shape CPU architecture."
     }
-  }  
+  }
 }
 
-
-
-
-resource "null_resource" "validate_ha_setup" {
-  count = local.invalid_ha_config ? 1 : 0
-
+resource "null_resource" "validate_instance_pool_compatibility" {
+  count = !var.rdma_enabled ? 1 : 0
   lifecycle {
     precondition {
-      condition     = !local.invalid_ha_config
-      error_message = "Error: Slurm HA configuration requires a shared NFS. Add NFS or create FSS (create_fss = true). "
+      condition     = contains(local.compatible_compute_shapes, var.instance_pool_shape)
+      error_message = "Selected compute image is not compatible with the compute node shape CPU architecture."
     }
   }
 }
