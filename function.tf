7bda967c635dcfea4e88089d7af80159daae6943
bruno-garbaccio <bruno.garbaccio@oracle.com>
2025-12-08 11:08:49 +0100

    use func.py
    avoid using a template as the code doesn't change and we use environment variables

--- function.tf
+++ function.tf
@@ -1,9 +1,3 @@
-resource "local_file" "updateFuncVariables" {
-  count      = var.use_OCI_generated_container ? 0 : 1
-  depends_on = [oci_queue_queue.queue]
-  source     = "func.py.tftpl"
-  filename   = "${path.module}/function/func.py"
-}
 
 resource "oci_identity_auth_token" "auth_token" {
   count       = var.use_OCI_generated_container || var.use_existing_auth_token ? 0 : 1
