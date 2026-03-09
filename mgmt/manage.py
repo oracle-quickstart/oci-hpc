0f97d4ab1650a2e8bb6725a9feb5ec902721430b
Matthias Wolf <matthias.wolf@oracle.com>
2026-03-09 16:27:24 +0100

    Add a `--debug` option and let click parse the environment variable. (#668)

--- mgmt/manage.py
+++ mgmt/manage.py
@@ -11,21 +11,25 @@ from lib.cli import subcommands
 
 
 @click.group()
-def cli():
-    """CLI for managing your application."""
-    pass
-
-if __name__ == "__main__":
-    debug_level=logging.INFO
-    if "MGMT_DEBUG" in os.environ.keys():
-        if os.environ["MGMT_DEBUG"] != 0 and os.environ["MGMT_DEBUG"].lower() != "false":
-            debug_level=logging.DEBUG
+@click.option(
+    "--debug",
+    envvar="MGMT_DEBUG",
+    is_flag=True,
+    help="Enable debug log output. Can also be activated via MGMT_DEBUG in the environment",
+)
+def cli(debug):
+    """CLI for managing your cluster."""
+    log_level = logging.INFO
+    if debug:
+        log_level = logging.DEBUG
     logging.basicConfig(
-        level=debug_level,
+        level=log_level,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
     logging.getLogger("oci").setLevel(logging.WARNING)
 
+
+if __name__ == "__main__":
     for subcommand in subcommands:
         cli.add_command(subcommand)
     cli()
