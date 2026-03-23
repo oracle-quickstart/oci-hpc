f37d2d5c314fd3e390a06a817deb72bbd431e57c
arnaudfroidmont <49765904+arnaudfroidmont@users.noreply.github.com>
2026-03-23 13:05:55 -0600

    Fix Marketplace images isssue with shape check

--- image.tf
+++ image.tf
@@ -1,5 +1,5 @@
 resource "oci_core_shape_management" "compute-shape" {
-    count = contains(local.compatible_compute_shapes, var.rdma_enabled ? var.cluster_network_shape : var.instance_pool_shape) ? 0 : 1
+    count = var.use_marketplace_image ? 0 : (contains(local.compatible_compute_shapes, var.rdma_enabled ? var.cluster_network_shape : var.instance_pool_shape) ? 0 : 1)
     compartment_id = var.targetCompartment
     image_id = local.compute_image
     shape_name = var.rdma_enabled ? var.cluster_network_shape : var.instance_pool_shape
@@ -7,7 +7,7 @@ resource "oci_core_shape_management" "compute-shape" {
 
 
 resource "oci_core_shape_management" "controller-shape" {
-    count = contains(local.compatible_controller_shapes, var.controller_shape) ? 0 : 1
+    count = var.use_marketplace_image_controller ? 0 : (contains(local.compatible_controller_shapes, var.controller_shape) ? 0 : 1)
     compartment_id = var.targetCompartment
     image_id = local.controller_image
     shape_name = var.controller_shape
